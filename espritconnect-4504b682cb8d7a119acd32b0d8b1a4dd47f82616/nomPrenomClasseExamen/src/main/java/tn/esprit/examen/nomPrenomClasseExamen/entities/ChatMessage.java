98a5c8763ded9045ee1d7eadc2e6905c0c766fd2
raedbrahem <raed.brahem@esprit.tn>
2025-03-09 12:16:50 +0100

    chat

---
+++ espritconnect-4504b682cb8d7a119acd32b0d8b1a4dd47f82616/nomPrenomClasseExamen/src/main/java/tn/esprit/examen/nomPrenomClasseExamen/entities/ChatMessage.java
@@ -0,0 +1,16 @@
+package tn.esprit.examen.nomPrenomClasseExamen.entities;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+
+}
